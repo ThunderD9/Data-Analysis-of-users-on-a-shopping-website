cd27d0028cbce84b4e9294d8378f8ed37d723b4c
ThunderD9 <87644895+ThunderD9@users.noreply.github.com>
2022-05-16 12:45:46 +0530

    Add files via upload

---
+++ website/website.py
@@ -0,0 +1,125 @@
+import csv
+import sys
+import pandas
+
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import confusion_matrix
+
+TEST_SIZE = 0.4
+
+
+def main():
+
+    # Check command-line arguments
+    if len(sys.argv) != 2:
+        sys.exit("Usage: python shopping.py data")
+
+    # Load data from spreadsheet and split into train and test sets
+    evidence, labels = load_data(sys.argv[1])
+    X_train, X_test, y_train, y_test = train_test_split(
+        evidence, labels, test_size=TEST_SIZE
+    )
+
+    # Train model and make predictions
+    model = train_model(X_train, y_train)
+    predictions = model.predict(X_test)
+    sensitivity, specificity = evaluate(y_test, predictions)
+
+    # Print results
+    print(f"Correct: {(y_test == predictions).sum()}")
+    print(f"Incorrect: {(y_test != predictions).sum()}")
+    print(f"True Positive Rate: {100 * sensitivity:.2f}%")
+    print(f"True Negative Rate: {100 * specificity:.2f}%")
+
+
+def load_data(filename):
+    """
+    Load shopping data from a CSV file `filename` and convert into a list of
+    evidence lists and a list of labels. Return a tuple (evidence, labels).
+
+    evidence should be a list of lists, where each list contains the
+    following values, in order:
+        - Administrative, an integer
+        - Administrative_Duration, a floating point number
+        - Informational, an integer
+        - Informational_Duration, a floating point number
+        - ProductRelated, an integer
+        - ProductRelated_Duration, a floating point number
+        - BounceRates, a floating point number
+        - ExitRates, a floating point number
+        - PageValues, a floating point number
+        - SpecialDay, a floating point number
+        - Month, an index from 0 (January) to 11 (December)
+        - OperatingSystems, an integer
+        - Browser, an integer
+        - Region, an integer
+        - TrafficType, an integer
+        - VisitorType, an integer 0 (not returning) or 1 (returning)
+        - Weekend, an integer 0 (if false) or 1 (if true)
+
+    labels should be the corresponding list of labels, where each label
+    is 1 if Revenue is true, and 0 otherwise.
+    """
+
+    months = {'Jan': 0,
+              'Feb': 1,
+              'Mar': 2,
+              'Apr': 3,
+              'May': 4,
+              'June': 5,
+              'Jul': 6,
+              'Aug': 7,
+              'Sep': 8,
+              'Oct': 9,
+              'Nov': 10,
+              'Dec': 11
+              }  # Creates a dict to map months to numbers
+
+    shopping = pandas.read_csv(filename)  # Reads the csv file
+    labels = shopping['Revenue']  # Creates a list(dataframe) of lables
+    evidence = shopping.drop(columns=['Revenue'])  # Removes the lables
+    evidence = evidence.replace(months)  # Changes the months to numbers
+    evidence['VisitorType'] = evidence['VisitorType'].apply(
+        lambda x: 1 if x == 'Returning_Visitor' else 0)  # Changes the VisitorType to 1 or 0
+
+    evidence['Weekend'] = evidence['Weekend'].apply(lambda x: 1 if x == 'True' else 0)  # Changes the Weekend to 1 or 0
+    labels = labels.apply(lambda x: 1 if x is True else 0)  # Changes the Labels to 1(True) or 0(False)
+
+    evidence_l = evidence.values.tolist()
+    labels_l = labels.values.tolist()  # Takes the dataframe values and puts them in a list
+
+    return (evidence_l, labels_l)
+
+
+def train_model(evidence, labels):
+    """
+    Given a list of evidence lists and a list of labels, return a
+    fitted k-nearest neighbor model (k=1) trained on the data.
+    """
+    neighbours = KNeighborsClassifier(n_neighbors=1)
+    neighbours.fit(evidence, labels)
+    return neighbours
+
+
+def evaluate(labels, predictions):
+    """
+    Given a list of actual labels and a list of predicted labels,
+    return a tuple (sensitivity, specificity).
+    Assume each label is either a 1 (positive) or 0 (negative).
+    `sensitivity` should be a floating-point value from 0 to 1
+    representing the "true positive rate": the proportion of
+    actual positive labels that were accurately identified.
+    `specificity` should be a floating-point value from 0 to 1
+    representing the "true negative rate": the proportion of
+    actual negative labels that were accurately identified.
+    """
+    ab, cd, qr, st = confusion_matrix(labels, predictions).ravel()
+    sens = st / (st + qr)
+    spec = ab / (ab + cd)
+
+    return sens, spec
+
+
+if __name__ == "__main__":
+    main()
